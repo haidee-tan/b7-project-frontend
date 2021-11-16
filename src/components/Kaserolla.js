2d356eee1792c7f0cb186498876d21117afac88f
haidee-tan <haideestan@gmail.com>
2021-11-16 17:37:32 +0800

    feat: added kaserolla.js

---
+++ src/components/Kaserolla.js
@@ -0,0 +1,27 @@
+// COMPONENTS
+import Home from "./Home";
+import Footer from "./Footer";
+import BeneficiaryPg from './BeneficiaryPg';
+import PostPg from './PostPg';
+
+
+const Kaserolla = (props) => {
+
+    return ( 
+        <>
+        <div>
+            <h1> CONNECTED</h1>
+        </div>
+
+        <div>
+            {/* <BeneficiaryPg /> */}
+        </div>
+            
+        <div>
+            {/* <PostPg /> */}
+        </div>
+        </>
+    )
+}
+
+export default Kaserolla
