48a2d5b51468dfaaec16891d4b965dc6485e2b2b
amlongalong <amlongalong@ymail.com>
2021-11-16 08:55:42 +0800

    Feat: initial set-up for front-end

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FightHungerApp from './components/FightHungerApp';
+
+
+function App () {
+  return (
+      <div>
+        < FightHungerApp />
+      </div>
+  )
+}
+ 
+export default App;
---
+++ src/components/FightHungerApp.js
@@ -0,0 +1,11 @@
+
+const FightHungerApp = (props) => {
+
+    return ( 
+        <>
+            <h1> CONNECTED</h1>
+        </>
+    )
+}
+
+export default FightHungerApp
